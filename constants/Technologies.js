66911eeba5c4e56d8156293d445be6c9b20b7e65
abdelazizeeeee <abdelaziz.naija@horizon-tech.tn>
2024-02-06 16:44:51 +0100

    fix bugs

--- constants/Technologies.js
+++ constants/Technologies.js
@@ -7,36 +7,38 @@ import { TbBrandNextjs, TbBrandVscode } from "react-icons/tb";
 export const technologies = [
   {
     category: "Programming Languges",
-    items: [ 
-      {name: "Python", icon: <FaPython size={32}/>},
-      {name: "JavaScript" , icon: <FaJsSquare size={32}/>},
+    items: [
+      { name: "Python", icon: <FaPython size={32} /> },
+      { name: "JavaScript", icon: <FaJsSquare size={32} /> },
+      { name: "NextJs", icon: <FaReact size={32} /> },
+      { name: "Typescrips", icon: <FaAngular size={32} /> },
     ]
   },
   {
     category: "Web Development",
-    items: [ 
+    items: [
     ]
   },
   {
     category: "Tools",
-    items: [ 
-      {name: "Git", icon: <FaGitSquare size={32}/>},
-      {name: "GitHub", icon: <FaGithub size={32}/>},
-      {name: "Postman" , icon: <SiPostman size={32}/>},
-      {name: "VsCode", icon: <TbBrandVscode size={32}/>},
+    items: [
+      { name: "Git", icon: <FaGitSquare size={32} /> },
+      { name: "GitHub", icon: <FaGithub size={32} /> },
+      { name: "Postman", icon: <SiPostman size={32} /> },
+      { name: "VsCode", icon: <TbBrandVscode size={32} /> },
     ]
   },
   {
     category: "OS",
-    items: [ 
-      {name: "Windows", icon: <FaWindows size={32}/>},
-      {name: "Linux", icon: <FaLinux size={32}/>},
+    items: [
+      { name: "Windows", icon: <FaWindows size={32} /> },
+      { name: "Linux", icon: <FaLinux size={32} /> },
     ]
   },
   {
     category: "Database",
-    items: [ 
-      {name: "MongoDB", icon: <DiMongodb  size={32}/>},
+    items: [
+      { name: "MongoDB", icon: <DiMongodb size={32} /> },
     ]
   },
 
